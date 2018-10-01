b9ab7f4c8bfeeec9c1e9305b4842b7da0a578534
navasu <neal.viswanath@gmail.com>
2018-10-01 16:53:38 -0700

    Create stack.py

---
+++ data_structures/stack.py
@@ -0,0 +1,36 @@
+class Node(object):
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+class Stack(object):
+    def __init__(self, top=None):
+        self.top = top
+
+    def push(self, val):
+        self.top = Node(val, self.top)
+
+    def pop(self):
+        if self.top is None:
+            return None
+        val = self.top.val
+        self.top = self.top.next
+        return val
+    
+    def peek(self):
+        return self.top.val if self.top is not None else None
+
+    def is_empty(self):
+        return self.peek() is None
+
+def main():
+    s = Stack()
+    print(s.is_empty())
+    s.push(1)
+    s.push(3)
+    s.push(5)
+    print(s.pop())
+    print(s.peek())
+    
+if __name__ == '__main__':
+    main()
